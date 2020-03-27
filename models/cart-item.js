fe8a042cee0db367760202ca3faa0b524ea038a0
Carl Olson <43393940+Hunter71a@users.noreply.github.com>
2020-03-27 17:09:00 -0400

    add cart.js to reducer and actions

---
+++ models/cart-item.js
@@ -0,0 +1,10 @@
+class CartItem {
+  constructor(quantity, productPrice, pruductTitle, sum) {
+    this.quantity = quantity;
+    this.productPrice = productPrice;
+    this.productTitle = productTitle;
+    this.sum = sum;
+  }
+}
+
+export default CartItem;
