40cc240e70ac7ed95c4a0bbf2808fd5d41f4bda5
Carl Olson <43393940+Hunter71a@users.noreply.github.com>
2020-03-27 15:09:05 -0400

    add custom fonts

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { ScrollView, View, Text, StyleSheet, Image, Button} from 'react-native';
-import {useSelector} from 'react-redux';
+import { ScrollView, View, Text, StyleSheet, Image, Button } from 'react-native';
+import { useSelector } from 'react-redux';
+import Colors from '../../constants/Colors';
 
 const ProductDetailScreen = props => {
   const productId = props.navigation.getParam('productId');
   const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId)
   );
 
-  return <View>
-    <Text>{selectedProduct.title}</Text>
-  </View>
+  return (
+  <ScrollView>
+    <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+    <View style={styles.actions}>  
+        <Button color={Colors.primary} title="Add to Cart" onPress={() => { }} />
+      </View>
+
+    <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+    <Text style={styles.description}>{selectedProduct.description}</Text>
+  </ScrollView>
+  );
 };
 
 ProductDetailScreen.navigationOptions = navData => {
@@ -18,6 +27,31 @@ ProductDetailScreen.navigationOptions = navData => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300
+  },
+  price: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 12,
+
+  },
+  description: {
+    fontFamily: 'open-sans',
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+
+
+});
 
 export default ProductDetailScreen;
